a9f4eec3f895b5a2eeffc77608c2ac28906e927f
jordiP28 <jorda2008pacheco@hotmail.com>
2026-02-09 15:07:09 -0500

    Calculadora terminada

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numeros): #suma
+    total = 0
+    for n in numeros:
+        total = total + n
+    return total
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numeros): #multiplicación
+    total = 1
+    for n in numeros:
+        total = total * n
+    return total
+
+
+def isiteven(numero): #par o impar
+    return numero % 2 == 0
+
+
+def isitaninteger(numero): #entero o no
+    return isinstance(numero, int)
+
+
+def main():
+    print("¡Hola 🫡!")
+    
+    # Ejemplos de uso (solo para probar)
+    numeros = [1, 10, 25, 4]
+    
+    print("Suma:", addmultiplenumbers(numeros))
+    print("Multiplicación:", multiplymultiplenumbers(numeros))
+    print("¿Es par 10?:", isiteven(10))
+    print("¿Es entero 24.5?:", isitaninteger(24.5))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+def main():
+    
+    nums = [1, 2, 3, 4]
+    
+    print("Suma:", addmultiplenumbers(nums))
+    print("Multiplicación:", multiplymultiplenumbers(nums))
+    print("¿Es par 4?:", isiteven(4))
+    print("¿Es entero 4.5?:", isitaninteger(4.5))
+
+
+if __name__ == "__main__":
+    main()
+
